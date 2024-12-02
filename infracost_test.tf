2ee785b06ec23c73c01a2bd44a5ceffc85812b06
Humzah Abdol <Humzah.Abdol@docusign.com>
2024-12-02 14:38:21 -0800

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -3,36 +3,79 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_linux_virtual_machine" "my_linux_vm" {
-  location = "eastus"
-  name = "test"
-  resource_group_name = "test"
-  admin_username = "testuser"
-  admin_password = "Testpa5s"
+variable "resource_count" {
+  description = "Number of resources to provision"
+  type        = number
+  default     = 10
+}
 
-  size = "Standard_F16s" # <<<<<<<<<< Try changing this to Standard_F16s_v2 to compare the costs
+# Provision multiple Linux Virtual Machines
+resource "azurerm_linux_virtual_machine" "my_linux_vm" {
+  count               = var.resource_count
+  location            = "eastus"
+  name                = "test-${count.index}"
+  resource_group_name = "test"
+  admin_username      = "testuser"
+  admin_password      = "Testpa5s"
+
+  size = "Standard_F16s" # Change to Standard_F16s_v2 for comparison
 
   tags = {
     Environment = "production"
-    Service = "web-app"
+    Service     = "web-app-${count.index}"
   }
 
   os_disk {
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    caching               = "ReadWrite"
+    storage_account_type  = "Standard_LRS"
   }
 
   network_interface_ids = [
-    "/subscriptions/123/resourceGroups/testrg/providers/Microsoft.Network/networkInterfaces/testnic",
+    "/subscriptions/123/resourceGroups/testrg/providers/Microsoft.Network/networkInterfaces/testnic-${count.index}",
   ]
 
   source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "16.04-LTS"
-    version = "latest"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
 }
+
+# Provision multiple App Service Plans
+resource "azurerm_service_plan" "my_app_service" {
+  count               = var.resource_count
+  location            = "eastus"
+  name                = "test-service-plan-${count.index}"
+  resource_group_name = "test_resource_group"
+  os_type             = "Windows"
+
+  sku_name    = "P1v2"
+  worker_count = 4 # Change to 8 for comparison
+
+  tags = {
+    Environment = "Prod"
+    Service     = "web-app-service-${count.index}"
+  }
+}
+
+# Provision multiple Linux Function Apps
+resource "azurerm_linux_function_app" "my_function" {
+  count                     = var.resource_count
+  location                  = "eastus"
+  name                      = "test-function-app-${count.index}"
+  resource_group_name       = "test"
+  service_plan_id           = "/subscriptions/123/resourceGroups/testrg/providers/Microsoft.Web/serverFarms/serverFarmValue"
+  storage_account_name      = "test-storage-${count.index}"
+  storage_account_access_key = "test"
+  site_config {}
+
+  tags = {
+    Environment = "Prod"
+    Service     = "function-app-${count.index}"
+  }
+}
+
 
 # resource "azurerm_service_plan" "my_app_service" {
 #   location = "eastus"
